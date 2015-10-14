5d015fa4eca5dbad6393e7f784fcb4e3d8a1ca32
Ben Hastings <bhastings@brightsign.biz>
2015-10-14 15:41:48 -0700

    Commit 2

--- src/BirthdayListViewer.java
+++ src/BirthdayListViewer.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 // in the list are shown on the JFrame in some fashion.
 public class BirthdayListViewer extends JFrame {
 	
+	// change
 	public static void main(String[] args) {
 		System.out.println("this is a test");
 	}
